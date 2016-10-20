6d52679df1fc7f8e289372cdda3d7419eb5d1a1d
Anton <Lantar23@yandex.ru>
2016-10-21 02:09:31 +0300

    Клиент переписан как отдельный проект со своим репозиторием

--- web/auth.php
+++
@@ -1,7 +0,0 @@
-<?
-
-print_r($_POST['login']);
-if ($_POST['newUser'])
-file_put_contents(__DIR__."/users.list", $_POST['login']." ".$_POST['pass']", FILE_APPEND);
-else
-echo 'запрос в базу данных...';
